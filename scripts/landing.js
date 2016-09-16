377c533c954abd5785a6374566334f36f5da04d7
Ricardo <rick@reddraft.com>
2016-09-15 21:56:28 -0500

    Add Animation with scroll event

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,40 @@
+//get points in selling-points
+var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function() {
-
-  var points = document.getElementsByClassName('point');
-
-  var revealPoint = function() {
-    for(var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.WevkitTransform = "scaleX(1) translateY(0)";
-    }
+//Animate every point in selling-points
+var animatePoints = function(points) {
+  //function that applies styles to a single point
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.WevkitTransform = "scaleX(1) translateY(0)";
   };
-  revealPoint();
+  //loop through pointsArray and apply the styling
+  for(var i = 0; i < pointsArray.length; i++) {
+    revealPoint(i);
+  }
+
 };
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  //distances we need to measure to figure out the number of pixels the user must scroll to trigger the animation
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  //print both scroll distance body.scrolltop for personal notes
+  console.log(scrollDistance);
+  console.log('-------');
+  console.log(document.body.scrollTop);
+
+
+  window.addEventListener('scroll', function(event) {
+    //if scroll to top is equal or greater than the distance we need to scroll activateanimate points
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+    }
+  });
+}
