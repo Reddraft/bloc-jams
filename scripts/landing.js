56238f61ff60f6045587a5d2f589454bb49ac433
Ricardo <rick@reddraft.com>
2016-10-17 21:25:20 -0500

    Refactor Jquery Landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,26 @@
-//get points in selling-points
-var pointsArray = document.getElementsByClassName('point');
 
 //Animate every point in selling-points
-var animatePoints = function(points) {
+var animatePoints = function() {
   //function that applies styles to a single point
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.WevkitTransform = "scaleX(1) translateY(0)";
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
   };
   //loop through pointsArray and apply the styling
-  for(var i = 0; i < pointsArray.length; i++) {
-    revealPoint(i);
-  }
+  $.each($('.point'), revealPoint);
+  // $('.point').each(revealPoint);
 
 };
 
-window.onload = function() {
+$(window).load(function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+  if ($(window).height() > 950) {
+      animatePoints();
   }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
   //distances we need to measure to figure out the number of pixels the user must scroll to trigger the animation
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
   //print both scroll distance body.scrolltop for personal notes
   console.log(scrollDistance);
@@ -31,10 +28,10 @@ window.onload = function() {
   console.log(document.body.scrollTop);
 
 
-  window.addEventListener('scroll', function(event) {
-    //if scroll to top is equal or greater than the distance we need to scroll activateanimate points
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-    }
+  $(window).scroll(function(event) {
+      //if scroll to top is equal or greater than the distance we need to scroll activate animate points
+      if ($(window).scrollTop() >= scrollDistance) {
+          animatePoints();
+      }
   });
-}
+});
